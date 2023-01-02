dc1de11fdd64703a9ee6e5535916dfbb2381d98e
Muhammad Zohaib Mohsin <zohaibmohsin30@gmail.com>
2023-01-02 16:42:56 +0500

    Two projects of piaic done

---
+++ Project one/calculator/index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node 
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+let stopper = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+async function wrapper() {
+    let title = chalkAnimation.rainbow('Welcome to my calculator');
+    await stopper();
+    title.stop();
+    console.log(chalk.red(`    _____________________
+    |  _________________  |
+    | | JO           0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|`));
+}
+//   calling function 
+await wrapper();
+repeater();
+async function showData() {
+    return inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "operator",
+            message: "What actions do you want to perform more \n",
+            choices: ["addition", "subtraction", "division", "multiplication"]
+        },
+        {
+            type: "number",
+            name: "num1",
+            message: "Kindly Enter number 1"
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Kindly Enter number 2"
+        }
+    ]).then((answers) => {
+        //Action for the calculator
+        if (answers.operator === "addition") {
+            console.log(`${answers.num1} + ${answers.num2} =`, answers.num1 + answers.num2);
+        }
+        else if (answers.operator === "subtraction") {
+            console.log(`${answers.num1} - ${answers.num2} =`, answers.num1 - answers.num2);
+        }
+        else if (answers.operator === "division") {
+            console.log(`${answers.num1} / ${answers.num2} =`, answers.num1 / answers.num2);
+        }
+        else if (answers.operator === "multiplication") {
+            console.log(`${answers.num1} * ${answers.num2} =`, answers.num1 * answers.num2);
+        }
+    });
+}
+async function repeater() {
+    do {
+        await showData();
+        var repeater_variable = await inquirer.prompt([
+            {
+                type: "input",
+                name: "repeat",
+                message: "If you to calculate again press Y/n"
+            }
+        ]);
+    } while (repeater_variable.repeat === "Yes" || repeater_variable.repeat === "Y" || repeater_variable.repeat === "yes" || repeater_variable.repeat === "y");
+}
---
+++ Project two/index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node 
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+// Global Variables
+let numbered;
+let answer;
+let count = 0;
+// function for pause of 3 seconds
+async function waiting() {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+}
+// welcome for displaying welcome message
+async function starter() {
+    let title = chalkAnimation.rainbow(`   ********                                      **   **                ****     **                     **                    
+   **//////**                                    /**  /**               /**/**   /**                    /**                    
+  **      //  **   **  *****   ******  ******   ******/**       *****   /**//**  /** **   ** ********** /**       *****  ******
+ /**         /**  /** **///** **////  **////   ///**/ /******  **///**  /** //** /**/**  /**//**//**//**/******  **///**//**//*
+ /**    *****/**  /**/*******//***** //*****     /**  /**///**/*******  /**  //**/**/**  /** /** /** /**/**///**/******* /** / 
+ //**  ////**/**  /**/**////  /////** /////**    /**  /**  /**/**////   /**   //****/**  /** /** /** /**/**  /**/**////  /**   
+  //******** //******//****** ******  ******     //** /**  /**//******  /**    //***//****** *** /** /**/****** //******/***   
+   ////////   //////  ////// //////  //////       //  //   //  //////   //      ///  ////// ///  //  // /////    ////// ///    `);
+    await waiting();
+    title.stop();
+    let another = chalkAnimation.rainbow(`All you need is to guess the number from 0 to 50`);
+    await waiting();
+    another.stop();
+    numbered = Math.floor(Math.random() * 50 + 1);
+}
+async function handler() {
+    return inquirer.prompt([
+        {
+            type: "number",
+            name: "val",
+            message: "Kindly Enter number and let Zohaib do the rest"
+        },
+    ]);
+    //   answer = prompt("Enter the number");
+}
+async function functionality() {
+    do {
+        answer = await handler();
+        count = count + 1;
+        if (answer.val > numbered) {
+            console.log(chalk.red(`Get a bit lower number`));
+        }
+        else if (answer.val < numbered) {
+            console.log(chalk.red(`Get a bit higher number`));
+        }
+    } while (answer.val !== numbered);
+    let final = chalkAnimation.rainbow(`This is the right answer.Your total number of guesses were ${count}`);
+    await waiting();
+    final.stop();
+}
+async function repeater() {
+    await inquirer.prompt([
+        {
+            name: "more",
+            type: "input",
+            message: "Do you want to play another time? y|n"
+        }
+    ]).then((data) => {
+        do {
+            functionality();
+            count = 0;
+            numbered = 0;
+        } while (data.input === "y" || data.input === "yes" || data.input === "Y" || data.input === "Yes");
+    });
+}
+// Calling the welcome message
+await starter();
+await functionality();
+repeater();
